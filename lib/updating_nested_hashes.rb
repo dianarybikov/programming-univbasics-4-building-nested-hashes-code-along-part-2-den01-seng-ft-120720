4ab1a6479c8c45a2d0b339312d09841362f2b545
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-21 19:35:35 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,10 +53,10 @@ def add_characters
       }
    }
 
-epic_tragedy[:additional_characters] <<
+epic_tragedy[ :additional_characters ] <<
   {name: "Prince Escalus" }
   
-epic_tragedy[:additional_characters] <<
+epic_tragedy[ :additional_characters ] <<
   {name: "Apothecary" } 
   
   
